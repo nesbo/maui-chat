e189fc13099a79bcfe2a9fa2898d055d21f846b3
Aleksandar Andonovski <aleksandar.andonovski@gmail.com>
2023-03-28 01:23:25 +0200

    Register services in app

--- Maui.Chat/Maui.Chat.App/MauiProgram.cs
+++ Maui.Chat/Maui.Chat.App/MauiProgram.cs
@@ -1,4 +1,7 @@
-﻿namespace Maui.Chat.App;
+﻿using Maui.Chat.Domain;
+using Maui.Chat.Domain.Handlers;
+
+namespace Maui.Chat.App;
 
 public static class MauiProgram
 {
@@ -12,7 +15,20 @@ public static class MauiProgram
                 fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                 fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
             });
+        
+        ConfigureServices(builder.Services);
 
         return builder.Build();
+    }
+    
+    private static void ConfigureServices(IServiceCollection services)
+    {
+        services.AddSingleton<IChatMessageCrypto, ChatMessageCryptoAes>();
+        services.AddScoped<ReceiveChatMessageCommandHandler>();
+        services.AddScoped<SendMessageCommandHandler>();
+        services.AddScoped<IClock, Clock>();
+        
+        // register publisher
+        // register repository
     }
 }
